e23fff692e57a22276ffaa509b7a38f8a3133835
James Hunt <james@niftylogic.com>
2015-06-28 15:31:34 -0400

    Initial commit

---
+++ lib/Muse.pm
@@ -0,0 +1,138 @@
+package Muse;
+
+use strict;
+use warnings;
+use File::Temp qw/tempfile/;
+use Digest::SHA;
+use Music::Tag;
+use base qw/Exporter/;
+our @EXPORT = qw/
+	waveform_checksum
+
+	fields metadata copy_metadata
+
+	convert
+/;
+
+my $LAST_ERROR;
+
+sub last_error
+{
+	$LAST_ERROR;
+}
+
+sub _run
+{
+	my ($command) = @_;
+	my $s = qx($command);
+	$? and $LAST_ERROR = $s;
+	return $? == 0;
+}
+
+sub _ext
+{
+	my ($f) = @_;
+	return lc($1) if $f =~ m/\.([^.]+)$/;
+	undef;
+}
+
+sub _base
+{
+	my ($f) = @_;
+	$f =~ s/\.([^.]+)$//;
+	return $f;
+}
+
+sub waveform_checksum
+{
+	my ($file) = @_;
+
+	my $sha = Digest::SHA->new('sha1');
+
+	(undef, my $tmp) = tempfile;
+	unlink $tmp;
+
+	my $ext = _ext($file)
+		or die "No file extension ($file)\n";
+
+	if ($ext eq 'ogg') {
+		_run(qq(oggdec -o $tmp "$file" 2>&1))
+			or die "OGG Vorbis decode of $file failed\n";
+		$sha->addfile($tmp);
+
+	} else {
+		die "Unrecognized file extension ($file)\n";
+	}
+
+	unlink $tmp;
+	return $sha->hexdigest;
+}
+
+sub fields
+{
+	qw/
+		album albumartist albumrating albumsortname
+		artist originalartist composer
+		asin upc
+		title compilation sortname genre
+		comment copyright rating
+		disc totaldisc disctitle
+		track totaltracks
+		releasedate year
+		mb_albumid mb_artistid mb_trackid mb_puid
+	/;
+}
+
+sub metadata
+{
+	my ($file) = @_;
+	my $info = Music::Tag->new($_, { quiet => 1 });
+	$info->get_tag;
+
+	my %metadata;
+	for (fields) {
+		$metadata{$_} = $info->get_data($_)
+			if $info->has_data($_);
+	}
+
+	\%metadata;
+}
+
+sub copy_metadata
+{
+	my ($from, $to) = @_;
+	my $src = get_metadata($from);
+	my $dst = Music::Tag->new($to, { quiet => 1 });
+
+	$dst->set_data($_, $src->{$_}) for keys %$src;
+	$dst->set_tag;
+}
+
+sub convert
+{
+	my ($from, $fmt, $force) = @_;
+	my %decode = (
+		ogg => qq(oggdec -Q -o "{{X}}.wav" "{{X}}.ogg"),
+		mp3 => qq(lame --decode "{{X}}.mp3" "{{X}}.wav"),
+	);
+	my %encode = (
+		flac => qq(flac -s "{{X}}.wav" && rm "{{X}}.wav"),
+	);
+
+	my $ext  = _ext($from);
+	my $base = _base($from);
+	return if -f "$base.$fmt" and !$force;
+
+	$decode{$ext} or die "Don't know how to decode .$ext files\n";
+	$encode{$fmt} or die "Don't know how to encode .$fmt files\n";
+
+	(my $cmd1 = $decode{$ext}) =~ s/{{X}}/_base($from)/ge;
+	(my $cmd2 = $decode{$fmt}) =~ s/{{X}}/_base($from)/ge;
+
+	_run(qq($cmd1 && $cmd2))
+		or return undef;
+
+	"$base.$fmt";
+}
+
+1;
